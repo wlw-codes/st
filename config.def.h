85b3cc57af859fc62bf8c4b1f0b9c12005bc673c
wlw-codes <will.lewis.w@gmail.com>
2021-12-21 16:12:40 +0000

    bump config.def

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font = "hack:size=12";
-static char *font2[] = { "fontawesome:size=12" };
+static char *font = "Hack:size=12";
+static char *font2[] = { "FontAwesome 5 Free:style=Solid:pixelsize=12" };
 static int borderpx = 2;
 
 /*
@@ -76,7 +76,7 @@ static unsigned int cursorthickness = 1;
 static int bellvolume = 0;
 
 /* default TERM value */
-char *termname = "st-256color";
+char *termname = "st";
 
 /*
  * spaces per tab
